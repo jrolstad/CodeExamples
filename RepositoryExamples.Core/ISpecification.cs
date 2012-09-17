1e848cbcb005c886e69088c9fbb23e184a511ad4
Josh Rolstad <jrolstad@gmail.com>
2012-09-17 14:28:56 -0700

    Adding specification interface

---
+++ RepositoryExamples.Core/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace RepositoryExamples.Core
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> IsSatisfied();
+    }
+}
