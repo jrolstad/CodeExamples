2acc5dbeeb31d5ad69e6111428389ae3e8e769a4
Josh Rolstad <jrolstad@gmail.com>
2012-09-21 11:40:33 -0700

    Adding examples for some good Ninject bindings

---
+++ NinjectBindingExamples/Modules/AuthenticationModule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.Web;
+using Ninject.Modules;
+
+namespace NinjectBindingExamples.Modules
+{
+    public class AuthenticationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IPrincipal>()
+              .ToMethod(context =>
+              {
+                  // Get the web user
+                  if (HttpContext.Current != null)
+                  {
+                      var identity = HttpContext.Current.User.Identity;
+                      return new GenericPrincipal(identity, new string[] { });
+                  }
+
+                  // Get the service user
+                  if (ServiceSecurityContext.Current != null)
+                  {
+                      var identity = ServiceSecurityContext.Current.WindowsIdentity;
+                      return new GenericPrincipal(identity, new string[] { });
+                  }
+
+                  // Get the local windows user
+                  var windowsIdentity = WindowsIdentity.GetCurrent();
+                  if(windowsIdentity == null)
+                      throw new ApplicationException("Unable to determine user identity");
+
+                  var principal = new WindowsPrincipal(windowsIdentity);
+                  return principal;
+              });
+        }
+    }
+}
---
+++ NinjectBindingExamples/Modules/LoggingModule.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Modules;
+using log4net;
+
+namespace NinjectBindingExamples.Modules
+{
+    public class LoggingModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILog>().ToMethod(context => LogManager.GetLogger(context.Binding.Target.GetType()));
+        }
+    }
+}
