296cfff56d7f70dda4cbb322f211800a0e223666
Josh Rolstad <jrolstad@gmail.com>
2012-10-02 10:04:06 -0700

    Adding couchbase implementation

---
+++ RepositoryExamples.Couchbase/CouchbaseRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Couchbase;
+using Enyim.Caching.Memcached;
+using Examples.Core.Repositories;
+
+namespace RepositoryExamples.Couchbase
+{
+    public class CouchbaseRepository:IRepository
+    {
+        private readonly CouchbaseClient _couchbase;
+
+        public CouchbaseRepository(CouchbaseClient couchbase)
+        {
+            _couchbase = couchbase;
+        }
+
+        public IQueryable<T> Find<T>() where T : IEntity
+        {
+            throw new System.NotImplementedException("Searching is not implemented in this version");
+        }
+
+        public T Get<T>(object key) where T : IEntity
+        {
+            var result = _couchbase.ExecuteGet<T>(key.ToString());
+
+            return result.Value;
+        }
+
+        public void Save<T>(T value) where T : IEntity
+        {
+            var result =_couchbase.ExecuteStore(StoreMode.Set, value.Id, value);
+        }
+
+        public void Delete<T>(T value) where T : IEntity
+        {
+            var result = _couchbase.ExecuteRemove(value.Id);
+        }
+    }
+}
---
+++ RepositoryExamples.Couchbase/CouchbaseRepositoryNinjectModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Couchbase;
+using Couchbase.Configuration;
+using Ninject.Modules;
+
+namespace RepositoryExamples.Couchbase
+{
+    public class CouchbaseRepositoryNinjectModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<CouchbaseClient>().ToMethod(context =>
+                {
+                    var config = new CouchbaseClientConfiguration
+                        {
+                            Bucket = "<bucket name to store object", 
+                            BucketPassword = "<password for the bucket"
+                        };
+                    config.Urls.Add(new Uri("http://localhost:8091/pools"));
+                    return new CouchbaseClient(config);
+                });
+        }
+    }
+}
