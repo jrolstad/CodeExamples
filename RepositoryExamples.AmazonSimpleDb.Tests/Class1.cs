771caf6e7b01bb3b71b720edefb230364036153f
Josh Rolstad <jrolstad@gmail.com>
2012-10-02 15:31:05 -0700

    Adding project for SimpleDB

---
+++ RepositoryExamples.AmazonSimpleDb.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.EntityClient;
+using System.Data.Linq;
+using System.Data.Objects;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using FizzWare.NBuilder;
+using NUnit.Framework;
+using System.Linq;
+
+namespace RepositoryExamples.AmazonSimpleDb.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        
+    }
+}
---
+++ RepositoryExamples.AmazonSimpleDb/AmazonSimpleDbRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Amazon.SimpleDB;
+using Examples.Core.Repositories;
+
+namespace RepositoryExamples.AmazonSimpleDb
+{
+    public class AmazonSimpleDbRepository:IRepository
+    {
+        private readonly AmazonSimpleDB _client;
+
+        public AmazonSimpleDbRepository(Amazon.SimpleDB.AmazonSimpleDB client)
+        {
+            _client = client;
+        }
+
+        public IQueryable<T> Find<T>() where T : IEntity
+        {
+
+        }
+
+        public T Get<T>(object key) where T : IEntity
+        {
+           
+        }
+
+        public void Save<T>(T value) where T : IEntity
+        {
+
+        }
+
+        public void Delete<T>(T value) where T : IEntity
+        {
+
+        }
+    }
+}
