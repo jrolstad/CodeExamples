fd586201a1fa76273caa06d406095f1fecda0251
Josh Rolstad <jrolstad@gmail.com>
2012-10-04 09:51:26 -0700

    Implementing SQS and adding tests for it

---
+++ Messaging.AmazonSqs/IMessageFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Messaging.AmazonSqs
+{
+    public interface IMessageFormatter
+    {
+        string Write<T>(T message);
+
+        T Read<T>(string body);
+    }
+}
